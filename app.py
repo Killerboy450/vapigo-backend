ec71a0d8ca2b3913afe53835015895f61447c353
Killerboy450 <hasansodawala786@gmail.com>
2026-02-15 10:33:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,33 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-import mysql.connector
+import psycopg2
+import os
 
-app=Flask(__name__)
+app = Flask(__name__)
 CORS(app)
 
-conn=mysql.connector.connect(host="localhost",user="root",password="1234",database="daily_cab_booking")
-cursor=conn.cursor()
+conn = psycopg2.connect(
+    host="yamabiko.proxy.rlwy.net",
+    database="railway",
+    user="postgres",
+    password="dFciWJiyDmyVPuspBZqfatWOBpPvELEn",
+    port="52932"
+)
 
-@app.route("/book_cab",methods=["POST"])
+cursor = conn.cursor()
+
+@app.route("/book_cab", methods=["POST"])
 def book_cab():
- data=request.json
- cursor.execute("INSERT INTO cab_booking(customer_name,mobile,pickup_location,drop_location) VALUES(%s,%s,%s,%s)",
- (data["name"],data["mobile"],data["pickup"],data["drop"]))
- conn.commit()
- return jsonify({"status":"success"})
 
-import os
+    data = request.json
+
+    cursor.execute(
+        "INSERT INTO cab_booking (customer_name, mobile, pickup_location, drop_location) VALUES (%s,%s,%s,%s)",
+        (data["name"], data["mobile"], data["pickup"], data["drop"])
+    )
+
+    conn.commit()
+
+    return jsonify({"status":"success"})
+
 app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
